0c26eabea59d3cc13a7468cac22ccc24c1bb4c45
Domi-ni <mtr.oliveira.me@gmail.com>
2023-12-08 12:27:48 -0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 ALIGN = "center"
-FONT = ('Courier', 15, 'normal')
+FONT = ('Courier', 15, 'bold')
 
 
 class ScoreBoard(Turtle):
@@ -8,18 +8,32 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score_tracker = 0
+        with open("high_score.txt") as high_score:
+            self.new_high_score = int(high_score.read())
         self.penup()
         self.color("White")
         self.goto(0, 270)
-        self.write(arg=f"Score: {self.score_tracker}", align=ALIGN, font=FONT)
+        self.write(arg=f"Score: {self.score_tracker} High Score:{self.new_high_score}", align=ALIGN, font=FONT)
         self.hideturtle()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(arg=f"Score: {self.score_tracker} High Score:{self.new_high_score}", align=ALIGN, font=FONT)
 
     def increase_score(self):
         self.score_tracker += 1
-        self.clear()
-        self.write(arg=f"Score: {self.score_tracker}", align=ALIGN, font=FONT)
+        self.update_scoreboard()
+
+    def reset_scoreboard(self):
+        if self.score_tracker > self.new_high_score:
+            self.new_high_score = self.score_tracker
+            with open("high_score.txt", mode="w") as high_score:
+                high_score.write(str(self.new_high_score))
+        self.score_tracker = 0
+        self.update_scoreboard()
 
     def game_over(self):
+        self.update_scoreboard()
+        self.clear()
         self.goto(0, 0)
         self.write(arg=f"GAME OVER", align=ALIGN, font=FONT)
-
